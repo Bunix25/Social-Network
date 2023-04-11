afd52b7b5c764ea949c920aa7df388192f64d329
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-11 12:17:30 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
+// Importing the User model from the User.js file
 const User = require('./User');
+
+// Importing the Thought model from the Thought.js file
 const Thought = require('./Thought');
 
+// Exporting an object containing both models for use in other parts of the application
 module.exports = { User, Thought };
