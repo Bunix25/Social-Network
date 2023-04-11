9fca7f57ab019eab42e86151fccfdd9800e40361
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-11 12:19:23 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+// Importing the Router object from the Express.js library
 const router = require('express').Router();
-// Import all of the API routes from /api/index.js 
+
+// Importing all of the API routes from the /api directory
 const apiRoutes = require('./api');
-// add prefix of `/api` to all of the api routes imported from the `api` directory
+
+// Adding a prefix of '/api' to all of the API routes imported from the 'api' directory
 router.use('/api', apiRoutes);
 
+// Creating a fallback route to handle any requests that do not match an existing route
 router.use((req, res) => res.send('Wrong route!'));
 
+// Exporting the router object to be used in other parts of the application
 module.exports = router;
