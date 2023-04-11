df7931a37daf525f7dd85e5d6ae8e9279cf99f9d
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-11 12:20:15 -0400

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,17 @@
+// Importing the Router object from the Express.js library
 const router = require('express').Router();
+
+// Importing the userRoutes object from the userRoutes.js file
 const userRoutes = require('./userRoutes');
+
+// Importing the thoughtRoutes object from the thoughtRoutes.js file
 const thoughtRoutes = require('./thoughtRoutes');
 
+// Adding a prefix of '/users' to all of the routes in the userRoutes object
 router.use('/users', userRoutes);
+
+// Adding a prefix of '/thoughts' to all of the routes in the thoughtRoutes object
 router.use('/thoughts', thoughtRoutes);
 
+// Exporting the router object to be used in other parts of the application
 module.exports = router;
