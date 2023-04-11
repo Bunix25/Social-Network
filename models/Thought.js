c6f77f1979db5ea240ba009fc4fdac10a0093fb5
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-11 12:14:46 -0400

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -63,13 +63,3 @@ const thoughtSchema = new Schema (
 )
 
 
-// get total count of friends
-thoughtSchema.virtual('reactionCount')
-.get(function() {
-    return this.reactions.length;
-})
-
-// create the User model using the UserSchema
-const Thought = model('Thought', thoughtSchema);
-// export the Thought model
-module.exports = Thought;
