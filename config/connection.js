6e8b80bcaa20805fe4c62983ddc44f5620fbb398
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-11 12:13:48 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
+// Importing the necessary modules from Mongoose
 const { connect, connection } = require('mongoose');
 
+// Setting the connection string to either the value of the environment variable "MONGODB_URI"
+// or to the default MongoDB connection URL 'mongodb://localhost:27017/socialDB'
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
 
+// Connecting to the MongoDB instance using the connection string and options
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Exporting the Mongoose connection object to be used in other parts of the application
 module.exports = connection;
